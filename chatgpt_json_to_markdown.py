fa99c0557fd1e3c2a21840ffd70c9255cf98ba79
Pent <sirpent@gmail.com>
2023-10-09 14:03:45 -0600

    Update chatgpt_json_to_markdown.py
    had GPT fix it up a bit

--- chatgpt_json_to_markdown.py
+++ chatgpt_json_to_markdown.py
@@ -1,6 +1,5 @@
 import json
 import os
-import sys
 import glob
 from datetime import datetime
 from tqdm import tqdm
@@ -12,16 +11,15 @@ def read_json_file(file_path):
 
 def process_conversations(data, output_dir, config):
     for conversation in tqdm(data, desc="Processing conversations"):
-        title = conversation["title"]
-        mapping = conversation["mapping"]
+        if not isinstance(conversation, dict):
+            continue  # Skip if conversation is not a dictionary
 
-        # Extract messages from the "mapping" key
+        title = conversation.get("title", "Untitled_Conversation")
+        mapping = conversation.get("mapping", {})
+        
         messages = [mapping[key]["message"] for key in mapping if mapping[key]["message"] is not None]
-
-        # Sort messages by their create_time
         messages.sort(key=lambda x: x["create_time"] if x["create_time"] is not None else float('-inf'))
 
-        # sanitize title to ensure it's a valid filename
         title = ''.join(c for c in title if c.isalnum() or c in [' ', '_']).rstrip()
         file_name = f"{config['file_name_format'].format(title=title.replace(' ', '_').replace('/', '_'))}.md"
         file_path = os.path.join(output_dir, file_name)
@@ -33,10 +31,13 @@ def process_conversations(data, output_dir, config):
 
             for message in messages:
                 author_role = message["author"]["role"]
-                content = message["content"]["parts"][0]
                 author_name = config['user_name'] if author_role == "user" else config['assistant_name']
-                if not config['skip_empty_messages'] or content.strip():
-                    f.write(f"**{author_name}**: {content}{config['message_separator']}")
+
+                if 'content' in message and 'parts' in message['content']:
+                    content_parts = message["content"]["parts"]
+                    content = " ".join(part for part in content_parts)
+                    if not config['skip_empty_messages'] or content.strip():
+                        f.write(f"**{author_name}**: {content}{config['message_separator']}")
 
 def main():
     config_path = "config.json"
